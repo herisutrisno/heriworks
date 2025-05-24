5fed1ad4919085c13a54cc201d96b4a6d52391b5
hsutrisno <rgbhsutrisno@gmail.com>
2025-05-25 00:19:17 +0800

    Initial commit for heriworks Astro site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Heri Sutrisno Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
